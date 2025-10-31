019ddbf5bc533b1ab185debc989b8d071afb238a
Haymanot <235764709+haymanotkefyalew@users.noreply.github.com>
2025-10-31 20:21:11 +0300

    feet(script): declare a function createDivs
    create a function which take one parameter the
     ratio and create ratio by ratio number of voxes

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const ratioButton=document.querySelector('#ratio')
-const resetButton=document.querySelector('#reset')
-const clearButton=document.querySelector('#check')
-const mianContainer=document.querySelector('.div-container')
-const colorInput=document.querySelector('#color-picker')
-const clearLabel=document.querySelector('#clear')
+const ratioButton=document.querySelector('#ratio');
+const resetButton=document.querySelector('#reset');
+const clearButton=document.querySelector('#check');
+const mianContainer=document.querySelector('.div-container');
+const colorInput=document.querySelector('#color-picker');
+const clearLabel=document.querySelector('#clear');
 const ratio=16;
+
+
+function createDivs(ratio){
+    mianContainer.innerHTML='';
+
+    for(let i=1;i<=(ratio * ratio);i++){
+        let divI=document.createElement('div');
+    }
+}
