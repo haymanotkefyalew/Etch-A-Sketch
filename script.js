e2d6ccca7518d94a18fdaf1f05c1cf6c3665dc2f
Haymanot <235764709+haymanotkefyalew@users.noreply.github.com>
2025-10-31 20:15:54 +0300

    feet: create a reference for the nodes in the html

---
+++ script.js
@@ -0,0 +1,7 @@
+const ratioButton=document.querySelector('#ratio')
+const resetButton=document.querySelector('#reset')
+const clearButton=document.querySelector('#check')
+const mianContainer=document.querySelector('.div-container')
+const colorInput=document.querySelector('#color-picker')
+const clearLabel=document.querySelector('#clear')
+const ratio=16;
